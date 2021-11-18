751455c6afa1387d6cc07c425c4344a52563917c
dmloprieno <72047010+dmloprieno@users.noreply.github.com>
2021-11-18 18:05:47 -0500

    First queries

---
+++ queries.sql
@@ -0,0 +1,21 @@
+--criteria
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- count emp meeting criteria
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--make new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT *
+FROM retirement_info
